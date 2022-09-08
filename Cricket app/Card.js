dbe96a508323130c0f2ade32c57644bd3b9b67ef
Shivam singh <88611640+Abdvirat@users.noreply.github.com>
2022-09-08 20:48:55 +0530

    Add files via upload

---
+++ Cricket app/Card.js
@@ -0,0 +1,76 @@
+import React from "react"
+
+const Card=()=>{
+    return(
+        <div className="main-div">
+            <div className="div1">
+                <h1>Jersy No 11</h1>
+                <h2>Virat Kohli</h2>
+                <h3>Score : 90</h3>
+                <h4>Wicket : 2</h4>
+            </div>
+            <div className="div1">
+                <h1>Jersy No 10</h1>
+                <h2>Dhoni</h2>
+                <h3>Score : 90</h3>
+                <h4>Wicket : 0</h4>
+            </div>
+            <div className="div1">
+                <h1>Jersy No 08</h1>
+                <h2>Suresh Raina</h2>
+                <h3>Score : 50</h3>
+                <h4>Wicket : 10</h4>
+            </div>
+            <div className="div1">
+                <h1>Jersy No 20</h1>
+                <h2>Virendra Sehwag</h2>
+                <h3>Score : 100</h3>
+                <h4>Wicket : 5</h4>
+            </div>
+            <div className="div1">
+                <h1>Jersy No 30</h1>
+                <h2>Gautam Gambhir</h2>
+                <h3>Score : 95</h3>
+                <h4>Wicket : 0</h4>
+            </div>
+            <div className="div1">
+                <h1>Jersy No 50</h1>
+                <h2>Harbhagan Singh</h2>
+                <h3>Score : 10</h3>
+                <h4>Wicket : 8</h4>
+            </div>
+            <div className="div1">
+                <h1>Jersy No 60</h1>
+                <h2>Yuraj Singh</h2>
+                <h3>Score : 87</h3>
+                <h4>Wicket : 4</h4>
+            </div>
+            <div className="div1">
+                <h1>Jersy No 04</h1>
+                <h2>Zaheer Khan</h2>
+                <h3>Score : 2</h3>
+                <h4>Wicket : 6</h4>
+            </div>
+            <div className="div1">
+                <h1>Jersy No 02</h1>
+                <h2>Shreeshant</h2>
+                <h3>Score : 0</h3>
+                <h4>Wicket : 2</h4>
+            </div>
+            <div className="div1">
+                <h1>Jersy No 03</h1>
+                <h2>Rohit Sharma</h2>
+                <h3>Score : 90</h3>
+                <h4>Wicket : 4</h4>
+            </div>
+            <div className="div1">
+                <h1>Jersy No 100</h1>
+                <h2>Sachin Tendulkar</h2>
+                <h3>Score : 30</h3>
+                <h4>Wicket : 0</h4>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
